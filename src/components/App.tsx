672817a7ab7b36ff0e2a39be43620ec1302d3a68
Adelia <adelgants@gmail.com>
2021-06-11 18:44:12 +0300

    init app

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'fontsource-roboto';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
